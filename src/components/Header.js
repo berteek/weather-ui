b21d34904447ff51b23681859533e6d9a9e4dc76
berteek <merzinalbert@gmail.com>
2022-06-21 18:33:51 +0400

    ESLint fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 function Header(props) {
+  const { title } = props;
+
   return (
     <header>
-      <h1>{props.title}</h1>
+      <h1>{title}</h1>
     </header>
   );
 }
+
+Header.defaultProps = {
+  title: "title_placeholder",
+};
+
+Header.propTypes = {
+  title: String,
+};
 
 export default Header;
